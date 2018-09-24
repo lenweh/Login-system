b7fdb12a8a35dc30513ef22e18959f0a54a7e95b
lenweh <38295462+lenweh@users.noreply.github.com>
2018-09-24 10:33:10 -0400

    Add files via upload

---
+++ includes/dbh.inc.php
@@ -0,0 +1,8 @@
+<?php
+
+$dbServername = "localhost";
+$dbUsername = "login";
+$dbPassword = "login";
+$dbName = "login_system";
+
+$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
